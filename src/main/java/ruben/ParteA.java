07d668e813dddfd56b18482e05ee18af9ea4c2fc
RubenVS <rub3nvs97@gmail.com>
2021-11-02 12:02:16 +0100

    Parte del código del jugador 1 y switch para elegir pares o nones

--- src/main/java/ruben/ParteA.java
+++ src/main/java/ruben/ParteA.java
@@ -4,10 +4,35 @@
  */
 package ruben;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ruben
  */
 public class ParteA {
+    
+    public static void main(String[] args) {
+        
+        int numero1, numero2, opcion;
+        Scanner t = new Scanner(System.in);
+        boolean pares, nones;
+        
+        System.out.println("Elija si prefiere pares o nones (Jugador 1) \n 1- Par \n 2-Nones");
+        opcion = t.nextInt();
+        
+        switch(opcion){
+            case 1:
+                System.out.println("Ha elegido pares");
+                System.out.println("Jugador 1 introduzca el número de dedos deseados");
+                numero1 = t.nextInt();
+                break;
+            case 2:
+                System.out.println("Ha elegido nones");
+                System.out.println("Jugador 1 introduzca el número de dedos deseados");
+                numero1 = t.nextInt();
+                break;
+        }
+    }
     
 }
