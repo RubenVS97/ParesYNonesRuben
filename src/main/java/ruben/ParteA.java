38177ee665da9e627f603bca03586d1df615e6bd
RubenVS <rub3nvs97@gmail.com>
2021-11-02 13:17:44 +0100

    Comentario del programa y eliminar algún error

--- src/main/java/ruben/ParteA.java
+++ src/main/java/ruben/ParteA.java
@@ -11,6 +11,7 @@ import java.util.Scanner;
  * @author ruben
  */
 public class ParteA {
+    //URL del github https://github.com/RubenVS97/ParesYNonesRuben/commits/master
 
     public static void main(String[] args) {
 
@@ -25,7 +26,37 @@ public class ParteA {
             switch (opcion) {
                 case 1:
                     System.out.println("Ha elegido pares");
+                    //Limpiamos buffer y pedimos el nombre del jugador por pantalla
+                    t.nextLine();
+                    System.out.println("¿Jugador 1 como te llamas?");
+                    nombre1 = t.nextLine();
+                    
                     //do while para filtrar el número de dedos 
+                    do {
+                        System.out.println(nombre1 + " introduce el número de dedos deseados");
+                        dedos1 = t.nextInt();
+                    } while (!(dedos1 > 1 && dedos1 < 10));
+                    
+                    //Limpiamos buffer y pedimos el nombre del jugador por pantalla
+                    t.nextLine();
+                    System.out.println("¿Jugador 2 como te llamas?");
+                    nombre2 = t.nextLine();
+                    
+                    do {
+                        System.out.println(nombre2 + " introduce el número de dedos deseados");
+                        dedos2 = t.nextInt();
+                    } while (!(dedos2 > 1 && dedos2 < 10));
+                    
+                    //if para saber quien ha ganado la partida, si la suma de ambas variables da 0 gana el jugador 1 si no el jugador 2
+                    if ((dedos1 + dedos2) % 2 == 0) {
+                        System.out.println(nombre1 + " ha ganado la partida");
+                    } else {
+                        System.out.println(nombre2 + " ha ganado la partida");
+                    }
+                    break;
+                    //Lo mismo que el case 1 pero en este caso ha elegido nones
+                case 2:
+                    System.out.println("Ha elegido nones");
                     t.nextLine();
                     System.out.println("¿Jugador 1 como te llamas?");
                     nombre1 = t.nextLine();
@@ -33,30 +64,7 @@ public class ParteA {
                         System.out.println(nombre1 + " introduce el número de dedos deseados");
                         dedos1 = t.nextInt();
                     } while (!(dedos1 > 1 && dedos1 < 10));
-
-                    t.nextLine();
-                    System.out.println("¿Jugador 2 como te llamas?");
-                    nombre2 = t.nextLine();
-                    do {
-                        System.out.println(nombre2 + " introduce el número de dedos deseados");
-                        dedos2 = t.nextInt();
-                    } while (!(dedos2 > 1 && dedos2 < 10));
-                    //if para saber quien ha ganado la partida
-                    if ((dedos1 + dedos2) % 2 == 0) {
-                        System.out.println(nombre1 + " ha ganado la partida");
-                    } else {
-                        System.out.println(nombre2 + " ha ganado la partida");
-                    }
-                    break;
-                case 2:
-                    System.out.println("Ha elegido nones");
-                    t.nextLine();
-                    System.out.println("¿Jugador 1 como te llamas?");
-                    nombre1 = t.nextLine();
-                    do {
-                        System.out.println(nombre1 + "introduce el número de dedos deseados");
-                        dedos1 = t.nextInt();
-                    } while (!(dedos1 > 1 && dedos1 < 10));
+                    
                     t.nextLine();
                     System.out.println("¿Jugador 2 como te llamas?");
                     nombre2 = t.nextLine();
@@ -70,6 +78,7 @@ public class ParteA {
                         System.out.println(nombre2 + " ha ganado la partida");
                     }
                     break;
+                    //case para salir 
                 case 3:
                     System.out.println("Ha seleccionado salir");
                     break;
--- src/main/java/ruben/ParteB.java
+++ src/main/java/ruben/ParteB.java
@@ -19,17 +19,18 @@ public class ParteB {
         int dedos1, dedos2, opcion;
         Scanner t = new Scanner(System.in);
         String nombre1;
-        System.out.println("Elija si prefiere pares o nones (Jugador 1) \n 1- Par \n 2-Nones");
+        System.out.println("Elija si prefiere pares o nones (Jugador 1) \n 1- Par \n 2-Nones \n 3-Salir");
         opcion = t.nextInt();
         //switch para elegir si prefieres pares o nones 
         do {
             switch (opcion) {
                 case 1:
                     System.out.println("Ha elegido pares");
-                    //do while para filtrar el número de dedos 
+                    //Limpiamos buffer y pedimos el nombre del jugador por pantalla
                     t.nextLine();
                     System.out.println("¿Jugador 1 como te llamas?");
                     nombre1 = t.nextLine();
+                    //do while para filtrar el número de dedos
                     do {
                         System.out.println(nombre1 + " introduce el número de dedos deseados");
                         dedos1 = t.nextInt();
@@ -39,13 +40,14 @@ public class ParteB {
                     dedos2 = r.nextInt(10) + 1;
                     System.out.println("El número es: " + dedos2);
 
-                    //if para saber quien ha ganado la partida
+                    //if para saber quien ha ganado la partida, si la suma de ambas variables da 0 gana el jugador 1 si no el jugador 2 
                     if ((dedos1 + dedos2) % 2 == 0) {
                         System.out.println(nombre1 + " ha ganado la partida");
                     } else {
                         System.out.println("La máquina ha ganado la partida");
                     }
                     break;
+                    //Lo mismo que el case 1 pero en este caso ha elegido nones
                 case 2:
                     System.out.println("Ha elegido nones");
                     t.nextLine();
@@ -65,6 +67,7 @@ public class ParteB {
                         System.out.println("La máquina ha ganado la partida");
                     }
                     break;
+                    //case para salir
                 case 3:
                     System.out.println("Ha seleccionado salir");
                     break;
